46b6b547a3f7a0cf131d075214a977dd8a46cb5f
Patrick <izerop@gmail.com>
2018-10-27 00:12:59 -0400

    feat: work

--- build.js
+++ build.js
@@ -7,73 +7,88 @@ const path = require('path');
 const moment = require('moment');
 const showdown  = require('showdown');
 
-const pages = [
-  {
-    page: 'home',
-    title: 'Home',
-    data: {
-      posts: []
-    }
-  },
-  {
-    page: 'about',
-    title: 'About'
-  },
-  {
-    page: 'pages',
-    title: 'Older Posts'
-  }
-];
-
-
-let file = path.join(__dirname, 'pages') + '/layout.ejs';
-console.log('render', file);
+let postData = [];
+const layoutFile = path.join(__dirname, 'pages') + '/layout.ejs';
 
 if(fs.existsSync('build')) {
   fs.removeSync('build');
 }
 fs.ensureDirSync('build');
 
-console.log('[PAGES]');
-pages.forEach(p => {
-  const { page, title, data } = p;
-  if (typeof data === 'undefined') {
-    p.data = null;
-  }
-  ejs.renderFile(file, p, function(err, str){
-    if(err) {
-      console.log(err);
-    } else {
-      let buildPath = `build/index.html`;
-      if(page !== 'home') {
-        fs.ensureDirSync(`build/${page}`);
-        buildPath = `build/${page}/index.html`
+
+const compilePages = async function() {
+  const pages = [
+    {
+      page: 'home',
+      title: 'Home',
+      data: {
+        posts: postData
       }
-      console.log('building', buildPath);
-      fs.writeFileSync(buildPath, str);
+    },
+    {
+      page: 'about',
+      title: 'About'
+    },
+    {
+      page: 'pages',
+      title: 'Older Posts'
     }
-  });
-});
+  ];
 
-// Build posts
+  console.log('[PAGES]');
 
-const srcPath = './posts';
-const distPath = './build';
-const converter = new showdown.Converter();
-console.log('[POSTS]');
-
-glob(`${srcPath}/**/*.json`, function (er, files) {
-  files.forEach( f => {
-    let { post } = fs.readJsonSync(f);
-    let slug = post.slug;
-    if(!slug) {
-      slug = slugify(post.title, { lower: true });
+  pages.forEach(p => {
+    const { page, title, data } = p;
+    if (typeof data === 'undefined') {
+      p.data = null;
     }
-    let buildPath = path.join(__dirname, slug);
-    let md = fs.readFileSync(`${srcPath}/${slug}/index.md`, 'utf8');
-    let html = converter.makeHtml(md);
-    fs.ensureDirSync(`${distPath}/${slug}`);
-    console.log(`building ${slug}`);
-    fs.writeFile(`${distPath}/${slug}/index.html`, html);
+    ejs.renderFile(layoutFile, p, function(err, str){
+      if(err) {
+        console.log(err);
+      } else {
+        let buildPath = `build/index.html`;
+        if(page !== 'home') {
+          fs.ensureDirSync(`build/${page}`);
+          buildPath = `build/${page}/index.html`
+        }
+        console.log('building', buildPath);
+        fs.writeFileSync(buildPath, str);
+      }
+    });
   });
-})
+}
+
+const compilePosts = async function() {
+  console.log('[POSTS]');
+
+  const srcPath = './posts';
+  const distPath = './build';
+  const converter = new showdown.Converter();
+
+  await glob(`${srcPath}/**/*.json`, (er, files) => {
+    files.forEach( f => {
+      let { post } = fs.readJsonSync(f);
+      let { title, slug, summary, createAt, updatedAt } = post;
+      if(!slug) {
+        slug = slugify(post.title, { lower: true });
+        post.slug = slug;
+      }
+      postData.push(post);
+      let buildPath = path.join(__dirname, slug);
+      let md = fs.readFileSync(`${srcPath}/${slug}/index.md`, 'utf8');
+      let html = converter.makeHtml(md);
+      fs.ensureDirSync(`${distPath}/${slug}`);
+      console.log(`building ${slug}`);
+      ejs.renderFile(layoutFile, {page: 'post', title, data: html}, (err, str) => {
+        if (err) { console.log(err); } else {
+          fs.writeFile(`${distPath}/${slug}/index.html`, str);
+        }
+      });
+    });
+    compilePages();
+  });
+}
+
+fs.copySync(path.join(__dirname, 'static'), path.join(__dirname, 'build', 'static'));
+
+compilePosts();
