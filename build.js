0d142edef948266409ba3711f7b7834392b98eb9
Patrick <izerop@gmail.com>
2019-01-13 14:01:28 -0500

    feat: add tagging

--- build.js
+++ build.js
@@ -1,13 +1,12 @@
-const bodyParser = require('body-parser');
 const fs = require('fs-extra');
 const ejs = require('ejs');
-const slugify = require('slugify')
 const glob = require('glob');
 const path = require('path');
 const moment = require('moment');
-var myMarked = require('marked');
-const date = require('date-and-time');
+const myMarked = require('marked');
 const fm = require('front-matter');
+
+/* Config Settings */
 
 myMarked.setOptions({
   renderer: new myMarked.Renderer(),
@@ -24,8 +23,22 @@ myMarked.setOptions({
   xhtml: false
 });
 
-const Typography = require('typography')
-const oceanBeachTheme = require('typography-theme-ocean-beach')
+/* CSS IN JS BECAUSE LOL */
+const Typography = require('typography');
+const oceanBeachTheme = require('typography-theme-ocean-beach');
+
+/* STRUCT */
+/*
+  [
+    name: 'programming': {
+      posts: ['']
+    }
+  ]
+*/
+let postTags = [];
+postTags['uncategorized'] = {
+  posts: []
+};
 
 oceanBeachTheme.overrideThemeStyles = options => ({
   a: {
@@ -35,9 +48,10 @@ oceanBeachTheme.overrideThemeStyles = options => ({
 
 const typography = new Typography(oceanBeachTheme)
 
+/* setup variables */
+const layoutFile = path.join(__dirname, 'src/pages') + '/layout.ejs';
 let postData = [];
 let rss = [];
-const layoutFile = path.join(__dirname, 'src/pages') + '/layout.ejs';
 
 if(fs.existsSync('build')) {
   fs.removeSync('build');
@@ -46,7 +60,6 @@ fs.ensureDirSync('build');
 
 
 const compilePages = async function() {
-
   // sort posts by date.
   postData.sort(function(a,b){
     // Turn your strings into dates, and then subtract them
@@ -58,13 +71,14 @@ const compilePages = async function() {
     return new Date(b.meta.date) - new Date(a.meta.date);
   });
 
-  // Take the top 6
+  // Take the top 6, figure stuff out later for pagination, etc....
   let latestPosts = postData.splice(0,6);
 
   const pages = [
     {
       page: 'home',
       title: 'Home',
+      summary: false,
       data: {
         posts: latestPosts
       }
@@ -72,20 +86,22 @@ const compilePages = async function() {
     {
       page: 'posts',
       title: 'Archive',
+      summary: false,
       data: {
         posts: postData
       }
     },
     {
       page: 'about',
-      title: 'About'
+      title: 'About',
+      summary: false
     }
   ];
 
   console.log('[PAGES] ');
 
   pages.forEach(p => {
-    const { page, title, data } = p;
+    const { page, data } = p;
 
     p.slug = page;
     if(page === 'home') {
@@ -113,7 +129,32 @@ const compilePages = async function() {
       }
     });
   });
-  console.log('Building RSS...');
+
+  console.log('[TAGS]');
+  fs.ensureDirSync(`build/tag`);
+  Object.keys(postTags).forEach( t => {
+    // const { page, data } = p;
+    p = {};
+    p.slug = t;
+    p.page = 'tag';
+    p.data = postTags[t];
+    p.data.posts.reverse();
+    p.title = `Posts tagged "${t}"`;
+    p.summary = false;
+    console.log(`Compiling tag: ${p.slug} with ${p.data.posts.length} posts`);
+    p.typography = typography;
+    ejs.renderFile(layoutFile, p, function(err, str){
+      if(err) {
+        console.log(err);
+      } else {
+        let buildPath = `build/tag/${t}/index.html`
+        fs.ensureDirSync(`build/tag/${t}`);
+        fs.writeFileSync(buildPath, str);
+      }
+    });
+  });
+  console.log('[RSS]');
+
   let buildDate = new Date().toUTCString();
   ejs.renderFile('src/pages/rss.ejs', {data: rss, buildDate}, function(err, str) {
     fs.writeFileSync('build/rss.xml', str);
@@ -147,9 +188,25 @@ const compilePosts = async function() {
       meta.pubDate = new Date(createDate).toUTCString();
       meta.postDate = momentDate;
       meta.slug = slug;
+      if(meta.tags) {
+        meta.tags.forEach((t) => {
+          t = t.toLowerCase();
+          if (postTags[t]) {
+            tag = postTags[t];
+          } else {
+            tag = {
+              posts: []
+            }
+          }
+          tag.posts.push(meta);
+          postTags[t] = tag;
+        });
+      } else {
+        postTags['uncategorized'].posts.push(meta);
+      }
       postData.push(meta);
       rss.push({meta, content: html});
-      ejs.renderFile(layoutFile, { typography: typography, page: 'post', slug: `posts/${slug}`, title, date: momentDate, data: html }, (err, str) => {
+      ejs.renderFile(layoutFile, { typography: typography, page: 'post', summary,  slug: `posts/${slug}`, title, date: momentDate, data: html, tags }, (err, str) => {
         if (err) { console.log(err); } else {
           fs.writeFileSync(`${buildPath}/index.html`, str, 'utf8');
         }
