ba8e2e333805869959224fa033b8799ee87c8d2b
Patrick <izerop@gmail.com>
2019-02-13 22:02:08 -0500

    feat: fix deployer

--- deploy.js
+++ deploy.js
@@ -9,37 +9,36 @@ const deploy = async () => {
     const dist = 'build';
     compress = await globby([`${dist}/**/*.html`, `${dist}/**/*.css`, `${dist}/**/*.js`, `${dist}/**/*.json`, `${dist}/**/*.xml`]);
     utils.output(`gzipping ${compress.length} files...`);
-    compress.forEach(f => {
-      compressFile(f);
-    });
-
-    function compressFile(filename, callback) {
+    let renameRemaining = compress.length;
+    compress.forEach(filename => {
       const compress = zlib.createGzip(),
           input = fs.createReadStream(filename),
           output = fs.createWriteStream(filename + '.gz');
 
       input.pipe(compress).pipe(output);
-
-      fs.moveSync(`${filename}.gz`, `${filename}`, { overwrite: true });
-
-      if (callback) {
-        output.on('end', callback);
-      }
-    }
+      output.on('close', () => {
+        fs.moveSync(`${filename}.gz`, `${filename}`, { overwrite: true });
+        if (--renameRemaining <= 0) {
+          initDeploy();
+        }
+      });
+    });
 
     utils.output(`done gzipping`);
   };
 
   await shrink();
 
-  utils.output(`Upload Meta Files`);
-  shell.exec("aws s3 cp build/ s3://patricksimpson.me/ --recursive --include='*' --exclude='*.html' --exclude='*.css' --exclude='*.js' --exclude='*.json' --exclude='*.xml' --exclude='*.DS_Store'");
-  utils.output(`Upload Base Files`);
-  shell.exec("aws s3 cp build/ s3://patricksimpson.me/ --recursive --content-encoding 'gzip' --exclude='*' --include='*.html' --include='*.css' --include '*.js' --include='*.json' --include='*.xml'");
-  utils.output(`Setting Expires`);
-  shell.exec("aws s3 cp build/ s3://patricksimpson.me/ --recursive --exclude='*' --include='*.css' --include='*.js' --include='*.svg' --include='*.png' --include='*.webmanifest' --acl public-read  --metadata-directive REPLACE --cache-control max-age=2592000");
-  utils.output(`Clearing CloudFront Cache`);
-  shell.exec("aws cloudfront create-invalidation --distribution-id=EHEGKY5SSPRMA --paths '/*'");
+  function initDeploy() {
+    utils.output(`Upload Meta Files`);
+    shell.exec("aws s3 cp build/ s3://patricksimpson.me/ --recursive --include='*' --exclude='*.html' --exclude='*.css' --exclude='*.js' --exclude='*.json' --exclude='*.xml' --exclude='*.DS_Store'");
+    utils.output(`Upload Base Files`);
+    shell.exec("aws s3 cp build/ s3://patricksimpson.me/ --recursive --content-encoding 'gzip' --exclude='*' --include='*.html' --include='*.css' --include '*.js' --include='*.json' --include='*.xml'");
+    utils.output(`Setting Expires`);
+    shell.exec("aws s3 cp build/ s3://patricksimpson.me/ --recursive --exclude='*' --include='*.css' --include='*.js' --include='*.svg' --include='*.png' --include='*.webmanifest' --acl public-read  --metadata-directive REPLACE --cache-control max-age=2592000");
+    utils.output(`Clearing CloudFront Cache`);
+    shell.exec("aws cloudfront create-invalidation --distribution-id=EHEGKY5SSPRMA --paths '/*'");
+  }
 }
 
 deploy();
